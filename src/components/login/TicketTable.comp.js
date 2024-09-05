668c7b6f523342c123eb7cf119aeb664e6f6add4
Pratham Vyawahare <116460437+Prathamvv13@users.noreply.github.com>
2024-09-05 12:33:07 +0530

    Added Breadcrumb

--- src/components/login/TicketTable.comp.js
+++ src/components/login/TicketTable.comp.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Table } from "react-bootstrap";
-export const TicketTable= () => {
+export const TicketTable= ({tickets}) => {
   return (
     <div>
         <Table striped bordered hover>
@@ -13,13 +13,16 @@ export const TicketTable= () => {
         </tr>
       </thead>
       <tbody>
-            <tr>
-              <td>1</td>
-              <td>2
-              </td>
-              <td>3</td>
-              <td>4</td>
-            </tr>
+        {tickets.length &&
+        tickets.map((row) =>(
+          <tr key={row.id}>
+            <td>{row.id}</td>
+            <td>{row.subject}</td>
+            <td>{row.status}</td>
+            <td>{row.addedAt}</td>
+          </tr>
+
+        ))}
           <tr>
             <td colSpan="4" className="text-center">
               No ticket show{" "}
--- src/page/Dashboard/Dashboard.page.js
+++ src/page/Dashboard/Dashboard.page.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { Container,Row,Col,Button } from 'react-bootstrap'
 import {TicketTable} from "../../components/login/TicketTable.comp"
+import tickets from "../../data/{} dummy-tickets.json"
+import {PageBreadcrumb} from "../../components/breadcrumb/Breadcrumb.comp"
 const Dashboard = () => {
   return (
     <Container>
+    <Row> 
+     <Col> 
+       <PageBreadcrumb page="Dashboard" />    
+     </Col>
+    </Row>
         <Row>
             <Col className='text-center mt-5 mb-2'>
             <Button variant='info' style={{fontSize: "2rem", padding: "10px 30px"}}>Add new Ticket</Button>
@@ -21,7 +28,7 @@ const Dashboard = () => {
       <hr />
       <Row>
         <Col className="recent-ticket">
-          <TicketTable/>
+          <TicketTable tickets={tickets}/>
         </Col>
       </Row>     
     </Container>
